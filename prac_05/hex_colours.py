2c8a6b0733515a009ad1868ee3254983fd7c7e24
Jack Francis <jack.francis@my.jcu.edu.au>
2021-09-05 22:18:37 +1000

    Alter colour name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical
 Hex colours in a dictionary
 """
 
-COLOUR_TO_HEX_CODE = {"Alice Blue   ": "#f0f8ff", "AntiqueWhite ": "#faebd7", "AntiqueWhite1": "#ffefdb",
+COLOUR_TO_HEX_CODE = {"AliceBlue    ": "#f0f8ff", "AntiqueWhite ": "#faebd7", "AntiqueWhite1": "#ffefdb",
                       "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
                       "aquamarine1  ": "#7fffd4", "aquamarine2  ": "#76eec6", "aquamarine4  ": "#458b74"}
 
