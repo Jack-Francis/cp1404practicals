d460c77d81fe94428285396114e592e8019c2841
Jack Francis <jack.francis@my.jcu.edu.au>
2021-09-26 23:23:36 +1000

    Amend code for dynamic_labels.py and dynamic_labels.kv

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -4,24 +4,28 @@ from kivy.uix.label import Label
 
 
 class DynamicLabelsApp(App):
-    """Main program - Kivy app to demo dynamic widget creation."""
+    """Main program - Kivy app to demo dynamic label creation."""
 
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        self.names = {"Lindsay Ward", "Jack Francis", "Ayla Gunawan", "Joseph Hallows", "Joshua Finch"}
+        self.names = ("Lindsay Ward", "Jack Francis", "Ayla Gunawan", "Joseph Hallows", "Joshua Finch")
+        print(self.names)
 
     def build(self):
         """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
         return self.root
 
     def create_labels(self):
         """Create labels from list and add them to the GUI."""
+        print("creating labels here", self.names)
         for name in self.names:
+            print(name)
             temp_label = Label(text=name)
-            self.root.ids.main.add_label(temp_label)
+            self.root.ids.main.add_widget(temp_label)
 
 
 DynamicLabelsApp().run()
